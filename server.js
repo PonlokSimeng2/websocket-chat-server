3f9b380969f2bf38541a7fcab1feff2da059b180
PonlokSimeng2 <121995214+PonlokSimeng2@users.noreply.github.com>
2025-10-23 14:30:34 +0700

    udate 8000

--- server.js
+++ server.js
@@ -154,7 +154,7 @@ function broadcastUserCount() {
   });
 }
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => {
   console.log(`WebSocket server is running on port ${PORT}`);
 });
